348e8b83a0e6168d2b81fc66c99047d827f765c8
Martin Sidorov <martin.sidorov27@gmail.com>
2020-06-27 17:56:07 +0300

    Format model file

--- models/models.go
+++ models/models.go
@@ -4,11 +4,11 @@ import "time"
 
 // Customer schema of the customer table
 type Customer struct {
-    ID	int64	`json:"id"`
-    FirstName	string	`json:"firstname"`
-    LastName	string	`json:"lastname"`
-	BirthDate	time.Time	`json:"birthdate"`
-	Gender	string	`json:"gender"`
-	Email	string	`json:"email"`
-	Address	string	`json:"address"`
+	ID        int64     `json:"id"`
+	FirstName string    `json:"firstname"`
+	LastName  string    `json:"lastname"`
+	BirthDate time.Time `json:"birthdate"`
+	Gender    string    `json:"gender"`
+	Email     string    `json:"email"`
+	Address   string    `json:"address"`
 }
